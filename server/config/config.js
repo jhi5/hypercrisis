352e50b9f3355dda9a08342afb05d48735c5ff4e
J. Munford <jaemunford@gmail.com>
2016-10-14 11:59:58 -0400

    fixed mlab deploy

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ module.exports = {
 		port: process.env.PORT || 3000
 	},
 	production: {
-		db: 'mongodb://jhi5:1337SNKj@',
+		db: 'mongodb://jhi5:1337SNKj@ds035826.mlab.com:35826/hypercrisis',
 		rootPath: rootPath,
 		port: process.env.PORT || 80
 	}
